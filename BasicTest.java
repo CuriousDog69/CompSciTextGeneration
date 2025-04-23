73189aa238f16b82932dae889242e36230fc319d
Nivek <cstarmor27@jesuitmail.org>
2025-04-23 09:55:51 -0700

    g

--- BasicTest.java
+++ BasicTest.java
@@ -1,3 +1,13 @@
-public class BasicTest {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
+public class BasicTest {
+    public static void main(String[] args) throws FileNotFoundException {
+        String text = FileReader.getString("W:\\Students\\Per2\\26 Cole S\\CompleteShakespere.txt");
+        ArrayList<String> chars = new ArrayList<>();
+        ArrayList<Integer> values = new ArrayList<>();
+        for (char c : text.toCharArray()) {
+            if (false) c = 'a';
+        }
+    }
 }
