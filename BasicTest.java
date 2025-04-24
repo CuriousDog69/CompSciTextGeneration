cc2fed401f7da924d8cb92c12a11bfc799581e44
CuriousDog69 <colliouresm@icloud.com>
2025-04-24 09:10:30 -0700

    g

--- BasicTest.java
+++ BasicTest.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class BasicTest {
     public static void main(String[] args) throws FileNotFoundException {
-        String text = FileReader.getString("W:\\Students\\Per2\\26 Cole S\\CompleteShakespere.txt");
-        ArrayList<Integer> values = new ArrayList<>();
-        Scanner KEYBOARD = new Scanner(System.in);
-        System.out.println("Length:");
-        int numLetters = KEYBOARD.nextInt();
-        String output = "";
-        for (int i = 0; i < numLetters; i++) {
-            output += text.charAt((int)(Math.random() * text.length()));
-        }
-        System.out.print(output);
+
     }
 }
--- FirstGen.java
+++ FirstGen.java
@@ -1,5 +1,18 @@
-public class FirstGen {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class FirstGen {
+    public static void main(String[] args) throws FileNotFoundException {
+        String text = FileReader.getString("W:\\Students\\Per2\\26 Cole S\\CompleteShakespere.txt");
+        ArrayList<Integer> values = new ArrayList<>();
+        Scanner KEYBOARD = new Scanner(System.in);
+        System.out.println("Length:");
+        int numLetters = KEYBOARD.nextInt();
+        String output = "";
+        for (int i = 0; i < numLetters; i++) {
+            output += text.charAt((int)(Math.random() * text.length()));
+        }
+        System.out.print(output);
     }
 }
